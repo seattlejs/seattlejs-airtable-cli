3f5d0d6b95178483ffbdeb9e4f4c73e4b299dbae
FX Wood <fxwood2@gmail.com>
2023-02-18 11:08:46 -0800

    lint and fix speakers

--- index.js
+++ index.js
@@ -2,15 +2,10 @@ const yargs = require('yargs/yargs')
 const mapSpeakers = require('./src/speakers').mapSpeakers
 
 const argv = yargs(process.argv.slice(2))
-    .boolean('s')
-    .describe('s', 'output speaker images and json to add to the website')
-    .alias('s','speakers')
-    .argv
+  .boolean('s')
+  .describe('s', 'output speaker images and json to add to the website')
+  .alias('s', 'speakers').argv
 
 if (argv.speakers) {
-    mapSpeakers()
+  mapSpeakers()
 }
-
-
-
-
